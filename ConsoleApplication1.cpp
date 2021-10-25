27d09f9550ab20f19f428f3e78e841f822603b6b
Voronin Dima <92313357+Voronin-Dmitrii@users.noreply.github.com>
2021-10-25 15:23:56 +0500

    random number

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int a = rand() % 99 + 1, x;
+	cout << "У вас есть 6 попыток угадать число от 1 до 99: ";
+	for (int i = 5; i > -1; i--) {
+		cin >> x;
+		if (x == a) {
+			cout << "Вы угадали число!";
+			break;
+		}
+		else
+			if (i > 0)
+			    cout << "Попыток осталось: " << i << endl << "Введите число: ";
+		if (i == 0)
+			cout << "Попытки кончились";
+	}
+	return 0;
+	
+}
+
